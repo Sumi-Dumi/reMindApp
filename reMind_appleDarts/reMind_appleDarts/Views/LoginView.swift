999140c703250a8ed5168885aa7de04704d76c91
ryosuke1832 <yamamoto.ryosuke.e@gmail.com>
2025-06-02 11:43:30 +1000

    refactoring for background view

--- reMind_appleDarts/reMind_appleDarts/Views/LoginView.swift
+++ reMind_appleDarts/reMind_appleDarts/Views/LoginView.swift
@@ -7,16 +7,6 @@ struct LoginView: View {
     var body: some View {
         ZStack {
             // Background gradient
-//            LinearGradient(
-//                gradient: Gradient(colors: [
-//                    Color.white,
-//                    Color.primaryGreen.opacity(0.08),
-//                    Color.pink.opacity(0.05)
-//                ]),
-//                startPoint: .top,
-//                endPoint: .bottom
-//            )
-//            .ignoresSafeArea()
             BackGroundView()
 
             VStack(spacing: 24) {
