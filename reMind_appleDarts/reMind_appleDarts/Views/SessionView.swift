a11580245a11e14a278dbe8bae93cbf537d44b37
DickyEvaldo <160503297+DickyEvaldo@users.noreply.github.com>
2025-06-02 11:15:55 +1000

    dicky changes

--- reMind_appleDarts/reMind_appleDarts/Views/SessionView.swift
+++ reMind_appleDarts/reMind_appleDarts/Views/SessionView.swift
@@ -9,22 +9,24 @@
 import SwiftUI
 import AVKit
 
+
 struct SessionView: View {
     @State private var progress: Float = 0.6
     
     var body: some View {
         ZStack {
+            Color.gray.ignoresSafeArea()
 
 //            VideoPlayer(player: AVPlayer(url: videoURL))
 //                .ignoresSafeArea()
                     // UI
             VStack {
-                // Progress
+                // Progress Bars
                 HStack(spacing: 6) {
                     ForEach(0..<5) { index in
                         Capsule()
                             .frame(height: 4)
-                            .foregroundColor(index < 1 ? .black : .black.opacity(0.3)) // adjust based on progress
+                            .foregroundColor(index < 1 ? .white : .black.opacity(0.3)) // adjust based on progress
                     }
                 }
                 .padding(.top, 20)
@@ -32,16 +34,18 @@ struct SessionView: View {
                 
                 Spacer()
                 
-                // Text
+                // Subtitle
                 Text("Now. Tell me 3 things you hear")
+                    .font(.system(size: 16, weight: .semibold))
                     .foregroundColor(.black)
-                    .font(.system(size: 18, weight: .medium))
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 24)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
                     .multilineTextAlignment(.center)
-                    .padding(.horizontal, 20)
+                Spacer().frame(height: 80)
                 
-                Spacer()
-                
-                // Button
+                // Button Allignment
                 HStack {
                     // keyboard
                     Button(action: {}) {
@@ -57,30 +61,31 @@ struct SessionView: View {
                     // mic
                     Button(action: {}) {
                         Image(systemName: "mic.fill")
-                            .foregroundColor(.black)
+                            .foregroundColor(Color(red: 220 / 255, green: 236 / 255, blue: 125 / 255))
                             .frame(width: 50, height: 50)
-                            .background(Color.blue)
+                            .background(.black)
                             .clipShape(Circle())
                     }
                     
                     Spacer()
                     
-                    // close
-                    Button(action: {}) {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.black)
-                            .frame(width: 40, height: 40)
-                            .background(Color.white.opacity(0.2))
-                            .clipShape(Circle())
-                    }
-                    
-                    // check
-                    Button(action: {}) {
-                        Image(systemName: "checkmark")
-                            .foregroundColor(.black)
-                            .frame(width: 40, height: 40)
-                            .background(Color.white.opacity(0.2))
-                            .clipShape(Circle())
+                    HStack{
+                        // close
+                        Button(action: {}) {
+                            Image(systemName: "delete.left.fill")
+                                .foregroundColor(.white)
+                                .frame(width: 40, height: 40)
+                        }
+                        
+                        // check
+                        Button(action: {}) {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.black)
+                                .frame(width: 28, height: 28)
+                                .background(Color(red: 220 / 255, green: 236 / 255, blue: 125 / 255))
+                                .clipShape(Circle())
+                        }
+                        
                     }
                 }
                 .padding(.horizontal, 30)
