f233fe5e93e2c057ee3c536d25015710ad080dda
ryosuke1832 <yamamoto.ryosuke.e@gmail.com>
2025-06-02 11:37:29 +1000

    modify video view

--- reMind_appleDarts/reMind_appleDarts/Views/VideoView.swift
+++ reMind_appleDarts/reMind_appleDarts/Views/VideoView.swift
@@ -16,18 +16,29 @@ struct CustomVideoPlayerView: UIViewControllerRepresentable {
         let controller = AVPlayerViewController()
         controller.player = player
         controller.showsPlaybackControls = false
-        controller.videoGravity = .resizeAspectFill
+        
         return controller
     }
 
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        DispatchQueue.main.async {
+            guard let superview = uiViewController.view.superview else { return }
+            
+            uiViewController.view.frame = CGRect(
+                x: 0,
+                y: 0,
+                width: superview.bounds.width,
+                height: superview.bounds.height
+            )
+            uiViewController.view.contentMode = .scaleAspectFill
+            uiViewController.videoGravity = .resizeAspectFill
+        }
     }
 }
 
-
 struct VideoView: View {
     @State var player = AVPlayer(url: Bundle.main.url(forResource: "sample_video", withExtension: "mp4")!)
-    
+
     var body: some View {
         CustomVideoPlayerView(player: player)
             .edgesIgnoringSafeArea(.all)
@@ -37,6 +48,9 @@ struct VideoView: View {
     }
 }
 
+
 #Preview {
     VideoView()
 }
+
+
